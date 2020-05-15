fb9965d506bc7d4049e062d0f4a8efbf84b5222d
SeanNeilTalbot <65349957+SeanNeilTalbot@users.noreply.github.com>
2020-05-15 10:06:48 +0200

    Add files via upload

---
+++ DisplayingText.py
@@ -0,0 +1,26 @@
+#DisplayingText.py
+
+#More text displaying coding examples
+
+print('The Capybara is the worlds largest rodent')
+print('The Capybara likes to line in groups.')
+print("The Capybara can swim.")
+
+print("It's a beautiful day in the neighborhood") #The ' in It's could read as end of text if using '' hence use of "".
+
+#For print to appear on multiple lines we can use multiple -print statements or;
+
+print('The Capybara is the worlds largest rodent\nThe Capybara likes to line in groups.\nThe Capybara can swim.')
+
+#\n insert new line
+#\t insert a tab
+
+#You can also use """...""" or '''...'''
+
+print("""The Capybara is the worlds largest rodent
+The Capybara likes to line in groups.
+The Capybara can swim.""")
+
+#You can use the \ to display a character without python reading the character what it was designed for.
+
+print('You could diplay a single \' like this without affecting the string.')

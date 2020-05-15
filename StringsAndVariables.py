b13e1888c5947b2422445b594e3d8311f56c4df8
Sean Talbot <65349957+SeanNeilTalbot@users.noreply.github.com>
2020-05-15 14:29:45 +0200

    Add files via upload

---
+++ StringsAndVariables.py
@@ -0,0 +1,16 @@
+#StringsAndVariables.py
+
+#Requesting input from user and assigning input to a variable.
+name = input("What is your name?")
+#display variable
+print(name)
+
+#Update value
+name = "Mary"
+#Display new value
+print(name)
+
+#Manipulating variable
+firstName = input("What is your first name? ")
+lastName = input("What is your last name? ")
+print('Hello ' + firstName + ' ' + lastName)

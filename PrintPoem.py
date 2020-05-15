60d0c2a83237289ebc71b726dac0c8e1f3f3c05e
Sean Talbot <65349957+SeanNeilTalbot@users.noreply.github.com>
2020-05-15 13:48:22 +0200

    Add files via upload

---
+++ PrintPoem.py
@@ -0,0 +1,9 @@
+#PrintPoem.py
+
+#Printing a poem over multiple lines. 
+
+print('''There once was a movie start icon
+who preffered to sleep with the light on.
+They learned how to code
+a device that sure glowed
+and lit up the night using Python!''')

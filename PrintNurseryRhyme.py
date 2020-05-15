ac61b0530a1ca578ab88a9918278ed29151adaf7
SeanNeilTalbot <65349957+SeanNeilTalbot@users.noreply.github.com>
2020-05-15 09:43:40 +0200

    Add files via upload

---
+++ PrintNurseryRhyme.py
@@ -0,0 +1,5 @@
+#PrintNurseryRhyme.py
+#Basic code on different print methods and practice the prin() method.
+
+print('Hickory Dickory Dock! The mouse ran up the clock.')
+print("Hickory Dickory Dock! The mouse ran up the clock.")

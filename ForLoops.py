7aa5551498ffc9def61a594d5c780dcca46a5ba9
Sean Talbot <65349957+SeanNeilTalbot@users.noreply.github.com>
2020-05-23 13:02:36 +0200

    Add files via upload

---
+++ ForLoops.py
@@ -0,0 +1,16 @@
+#ForLoops
+
+#We take a look at for loops and we use the turtle module
+
+#Turtle draws lines on the screen and helps us learn and practice in a entertaining way.
+import turtle
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.color('green')
+turtle.forward(50)
+turtle.color('red')
+
+for steps in range(4):
+    turtle.forward(100)
+    turtle.right(90)

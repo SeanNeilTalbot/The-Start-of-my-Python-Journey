126bd658e0023a72883c7147edcd0a6980a0acff
Sean Talbot <65349957+SeanNeilTalbot@users.noreply.github.com>
2020-06-11 13:42:02 +0200

    Add files via upload

---
+++ CreatingFunctions.py
@@ -0,0 +1,21 @@
+#CreatingFunctions.py
+
+#We take a look at creating your own functions and about passing in parameters into your functions.
+
+#Creating a function
+    #Keyword def = define
+    #Function name - Must have meaning.
+    #Example;
+        #def printMessage():
+            #print('Hello World')
+            #return
+
+#Calling the function
+    #Use its name
+        #printMessage()
+
+def printMessage():
+    print('Hello World')
+    return
+
+printMessage()
